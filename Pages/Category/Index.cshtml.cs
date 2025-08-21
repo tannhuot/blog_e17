8429b7704741b2c8e7d8e4e9ffa263a767453f37
huot <khouvtannhuot168@gmail.com>
2025-08-21 20:41:19 +0700

    init project

---
+++ Pages/Category/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace blog_e17.Pages.Category
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
